e8b0aff5285eed8a8b0eb788dcd8dcd5fcb7c53c
Pat Blau <that_email_here>
2025-10-31 19:07:50 -0500

    Initial secure setup

--- MyApp/obj/Debug/net9.0/App.AssemblyInfo.cs
+++ MyApp/obj/Debug/net9.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e07fdfa6135d86f7e29bcdc49845480bb7ae2662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53fb13019a3dfcdfef4b185c0a1b258022ad196b")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
